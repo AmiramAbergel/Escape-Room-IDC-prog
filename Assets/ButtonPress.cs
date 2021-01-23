9b4f78f88973ae9789a72f350f47505adaf643b1
Amiram Abergel <Amiram.ab@gmail.com>
2021-01-23 21:55:40 +0200

    fix xr rig

---
+++ Assets/ButtonPress.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+    public Transform downTransform;
+    public Transform buttonMesh;
+
+    public GameObject lockpad;
+    public string key;
+
+    private void OnTriggerEnter(Collider other){
+        if(other.tag == "player"){
+            lockpad.SendMessage("KeyEntry", key);
+            buttonMesh.position = downTransform.position;
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other){
+        if(other.tag == "Player"){
+            buttonMesh.localPosition = Vector3.zero;
+        }
+    }
+
+}
